e2dfd96d868cecacc548003ed24261fa13cb5593
Jonathan Bardo <bardo.jonathan@gmail.com>
2014-06-19 12:43:27 -0400

    First release

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-wp-always-active-plugins
-========================
+# Always-Active-Plugins
+
+Force activation of provided plugins, also preventing their deactivation
+
+**Contributors:** [westonruter](http://profiles.wordpress.org/westonruter), [jonathanbardo](http://profiles.wordpress.org/jonathanbardo)
+**Requires at least:** 3.5 
+**Tested up to:** 3.9  
+**Stable tag:** trunk (master)  
+**License:** [GPLv2 or later](http://www.gnu.org/licenses/gpl-2.0.html)  
+
+## Description ##
+
+Not every plugin work in the mu-plugins folder. This plugin can circumvent that issue by forcing the activation of certain plugins in the plugins directory.
+
+This plugin must be loaded in the mu-plugins directory. Once it is loaded, one can implement a filter to force network activate a plugin network wise.
+
+```php
+add_filter(
+	'auto_activated_network_required_plugins', 
+	function( $plugins ) {
+		$plugins[] = 'PLUGIN_FOLDER/PLUGIN_FILE.php';
+		return $plugins; 
+	}
+);
+```
---
+++ always-active-plugins.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: Force activation of provided plugins, also preventing their deactivation
+Version: 0.1
+Author: Weston Ruter, Jonathan Bardo
+*/
+
+class Always_Active_Plugins {
+
+	/**
+	 * @action muplugins_loaded
+	 */
+	static function setup() {
+		self::activate_plugins();
+		add_filter( 'plugin_action_links', array( __CLASS__, 'filter_plugin_action_links_to_disable_blog_deactivation' ), 10, 4 );
+		add_filter( 'network_admin_plugin_action_links', array( __CLASS__, 'filter_plugin_action_links_to_disable_network_deactivation' ), 10, 4 );
+	}
+
+	static function activate_plugins() {
+		require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+
+		$required_plugins = apply_filters( 'auto_activated_required_plugins', array() );
+		$network_required_plugins = apply_filters( 'auto_activated_network_required_plugins', array() );
+		$required_plugins = array_merge( $required_plugins, $network_required_plugins );
+
+		foreach ( $required_plugins as $plugin ) {
+			$is_network = is_network_only_plugin( $plugin ) || in_array( $plugin, $network_required_plugins );
+			$result = activate_plugin( $plugin, '', $is_network );
+			if ( is_wp_error( $result ) ) {
+				wp_die( sprintf( __( '%1$s: %2$s' ), $plugin, $result->get_error_message() ) );
+			}
+		}
+	}
+
+	/**
+	 * @action plugin_action_links
+	 */
+	static function filter_plugin_action_links_to_disable_blog_deactivation( $actions, $plugin_file, $plugin_data, $context ) {
+		$plugins = apply_filters( 'auto_activated_required_plugins', array() );
+
+		if ( in_array( $plugin_file, $plugins ) ) {
+			unset( $actions['edit'] );
+			unset( $actions['deactivate'] );
+		}
+
+		return $actions;
+	}
+
+	static function filter_plugin_action_links_to_disable_network_deactivation( $actions, $plugin_file, $plugin_data, $context ) {
+		$plugins = apply_filters( 'auto_activated_network_required_plugins', array() );
+		
+		if ( in_array( $plugin_file, $plugins ) ) {
+			unset( $actions['edit'] );
+			unset( $actions['deactivate'] );
+		}
+		
+		return $actions;
+	}
+
+}
+
+add_action( 'muplugins_loaded', array( 'Always_Active_Plugins', 'setup' ) );
